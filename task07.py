a472fc0895c3e8f1d6d4778fdb11d2df9ca174b1
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-06-11 09:03:41 +0500

    Bajarildi

--- task07.py
+++ task07.py
@@ -1,8 +1,8 @@
-eng_katta = 0
+eng_katta = None
 
 for i in range(5):
     son = int(input(f"{i+1}-sonni kiriting: "))
-    if eng_katta == 0 or son > eng_katta:
+    if eng_katta == None or son > eng_katta:
         eng_katta = son
 
 print(f"Eng katta son: {eng_katta}")
